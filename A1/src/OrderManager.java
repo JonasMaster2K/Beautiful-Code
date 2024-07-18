2446b000b6c87af95f4932696e7374d01c12b309
JonasMaster2K <70891950+JonasMaster2K@users.noreply.github.com>
2024-07-18 09:56:42 +0200

    Aufgabe

---
+++ A1/src/OrderManager.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class OrderManager {
+    private ArrayList<Order> orders;
+
+    public OrderManager() {
+        orders = new ArrayList<>();
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public void printOrders() {
+        for (Order order : orders) {
+            order.printOrder();
+            System.out.println("------------------------------------");
+        }
+    }
+}
